996d32231d80547e104f03648fe7e8e5e7f489a5
Rasmus Johansson <rasmus@demosgis.net>
2020-04-27 12:41:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ function renderPlaces(places) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', './assets/3DmodelHaga.gltf');
-        model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '1.0 1.0 1.0');
 
