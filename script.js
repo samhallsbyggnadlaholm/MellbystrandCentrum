cb1defa336729a353bcd46049acc7deb1fac21ce
Rasmus Johansson <rasmus@demosgis.net>
2020-04-28 09:46:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function renderPlaces(places) {
         model.setAttribute('gltf-model', './assets/3DmodelHaga.gltf');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.1 0.1 0.1');
-		model.setAttribute('position', '0 30 0');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
