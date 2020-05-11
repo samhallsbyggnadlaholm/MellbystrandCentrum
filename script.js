0cdd64f549266131f1e73355eec58541902a313b
Rasmus Johansson <rasmus@demosgis.net>
2020-05-11 12:09:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function renderPlaces(places) {
         model.setAttribute('gltf-model', './assets/EkKartaModel.fbx');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('scale', '0.1 0.1 0.1');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
