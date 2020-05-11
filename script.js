863e75d0b21bb052cd1e5b087007f96cc9cdd0e4
Rasmus Johansson <rasmus@demosgis.net>
2020-05-11 13:46:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function renderPlaces(places) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model-model', 'https://samhallsbyggnadlaholm.github.io/MellbystrandCentrum/assets/EkKartaModel.glb');
-        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('rotation', '0 0 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '1 1 1');
 
