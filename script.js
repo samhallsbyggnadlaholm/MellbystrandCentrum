701b94337b23ad31f08c499bfa5541bdb4380d3d
Rasmus Johansson <rasmus@demosgis.net>
2020-05-13 13:46:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Kartor',
             location: {
-                 lat: 56.228768,
-                 lng: 12.830363,
+                 lat: 56.231527,
+                 lng: 12.832376,
             },
         },
     ];
