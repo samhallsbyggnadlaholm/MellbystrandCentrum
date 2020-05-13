92048ee6e4fd464b87337f08e28a550b50b7f52e
Rasmus Johansson <rasmus@demosgis.net>
2020-05-13 12:29:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
+    button.innerText = 'Kartor';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
